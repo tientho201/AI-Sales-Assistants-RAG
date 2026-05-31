3a9132365814c596c94c8678e792e0d031c0085f
tientho201 <tientho2012004@gmail.com>
2026-05-31 23:32:27 +0700

    feat: add Qdrant and Neo4j services to docker-compose and update configuration schema

--- app/config.py
+++ app/config.py
@@ -16,6 +16,19 @@ class Settings(BaseSettings):
     OPENAI_CHAT_MODEL: str = os.getenv(
         "OPENAI_CHAT_MODEL", "gpt-4o-mini")
 
+    QDRANT_URL: str = os.getenv("QDRANT_URL", "")
+    QDRANT_API_KEY: str = os.getenv("QDRANT_API_KEY", "")
+    QDRANT_COLLECTION_NAME: str = os.getenv("QDRANT_COLLECTION_NAME", "")
+    EMBEDDING_DIMENSION: int = os.getenv("EMBEDDING_DIMENSION", 1536)
+
+    NEO4J_URI: str = os.getenv("NEO4J_URI", "")
+    NEO4J_USERNAME: str = os.getenv("NEO4J_USERNAME", "")
+    NEO4J_PASSWORD: str = os.getenv("NEO4J_PASSWORD", "")
+
+    VECTOR_TOP_K: int = os.getenv("VECTOR_TOP_K", 5)
+    GRAPH_TOP_K: int = os.getenv("GRAPH_TOP_K", 5)
+    TOP_K_HYBRID_RETRIEVER: int = os.getenv("TOP_K_HYBRID_RETRIEVER", 5)
+
     # Set model config
     model_config = SettingsConfigDict(
         env_file=".env",
